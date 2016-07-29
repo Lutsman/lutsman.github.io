27c4fa25f0ee27dc43ad6171dd324b0958cc823d
Dimitry Loutsenko <333333y@gmail.com>
2016-07-29 22:20:07 +0545

    socials

--- js/script.js
+++ js/script.js
@@ -316,6 +316,18 @@ $(document).ready(function () {
 		}
 	})();
 	
-	
+	/*Socials*/
+	/*Like*/
+	(function(){
+		(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.7";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+	})();
+
+
 
 });
