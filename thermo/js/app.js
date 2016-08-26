0aeba860057e552f206f17501860d8a3339c4c93
Dimitry Loutsenko <333333y@gmail.com>
2016-08-26 20:10:34 +0545

    доработки, ровняем колонки

--- thermo/js/app.js
+++ thermo/js/app.js
@@ -573,6 +573,7 @@ $(document).ready(function () {
         $fancyHonorsGal.fancybox({
             padding: [0, 0, 0, 0],
             margin: [0, 0, 0, 0],
+            maxHeight: '80%',
             minHeight: '80%',
             tpl: {
                 closeBtn: fancyCloseBtn
@@ -670,6 +671,35 @@ $(document).ready(function () {
        })
    })();
 
+    /*Compare_table same column height*/
+    (function(){
+        setSameRowHeight();
+        $(window).resize(setSameRowHeight);
+
+        function setSameRowHeight() {
+            var $table = $('.compare__table');
+            var $columns = $table.find('.table-column');
+
+            for (var i = 1; i < $columns[0].children.length; i++) {
+                var maxHeight = 0;
+
+                for (var j = 0; j < $columns.length; j++) {
+                    $columns[j].children[i].style.height = '';
+                    var currRowHeight = $columns[j].children[i].offsetHeight;
+                    maxHeight = maxHeight > currRowHeight ? maxHeight : currRowHeight;
+                }
+
+                for (var k = 0; k < $columns.length; k++) {
+                    //если это колонка с именами, то к первому и последнему блоку добавляем 1px из-за разницы в границе
+                    if (k === 0 && (i === 1 || i === $columns[0].children.length - 1)) {
+                        $columns[k].children[i].style.height = maxHeight + 1 + 'px';
+                    } else {
+                        $columns[k].children[i].style.height = maxHeight + 'px';
+                    }
+                }
+            }
+        }
+    })();
 
 
 
