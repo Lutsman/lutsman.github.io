c063c8115c86b424c8f239ec1df77eadb2e17b25
Lutsman_win <333333y@gmail.com>
2019-01-17 20:31:18 +0545

    sber react deploy

---
+++ precache-manifest.a768801850d87b7409f9eb6fbda523a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1e38b9c91c4cebf74899",
+    "url": "/static/css/main.e85648d4.chunk.css"
+  },
+  {
+    "revision": "b2bd8614408a87223406051cb7c0ec1f",
+    "url": "/static/media/money.b2bd8614.png"
+  },
+  {
+    "revision": "77bb77d657aeb5d5d0b1",
+    "url": "/static/js/1.77bb77d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "216b28df88259af21930c9539331d5bf",
+    "url": "/static/media/icon-arrow.216b28df.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1e38b9c91c4cebf74899",
+    "url": "/static/js/main.1e38b9c9.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0d331d1111bc6850d482d2464c2093c5",
+    "url": "/static/media/bg_light.0d331d11.png"
+  },
+  {
+    "revision": "9724af2fd30572f7485af3213d62f144",
+    "url": "/static/media/btn-power-circles.9724af2f.png"
+  },
+  {
+    "revision": "77bb77d657aeb5d5d0b1",
+    "url": "/static/css/1.6d090ba7.chunk.css"
+  },
+  {
+    "revision": "e56ca17924e3ef9f2abdb37392d5414b",
+    "url": "/index.html"
+  }
+];
